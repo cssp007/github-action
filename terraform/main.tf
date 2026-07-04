587c73c631715802fefa311bc9a86f025e0b1c5f
Somnath Pandey <44345876+cssp007@users.noreply.github.com>
2026-07-04 18:23:21 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,11 +65,4 @@ resource "aws_security_group" "sonarQ_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
