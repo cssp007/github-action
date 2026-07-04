9835b7b7f21453ba38dab962d1a5d73ef6f2dda1
Somnath Pandey <44345876+cssp007@users.noreply.github.com>
2026-07-04 18:13:53 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,11 @@
-terraform {
-  backend "s3" {}
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+      Project     = "Demo"
+    }
+  }
 }
